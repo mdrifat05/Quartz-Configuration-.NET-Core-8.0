0c57bc304690251a09891e8eceedc60e7b8b375d
Md. Rifat Hossen <hossen.mdrifat20@gmail.com>
2023-12-23 00:45:01 +0600

    quartz configuration done

---
+++ Infrastructure/LoggingBackgroundJob.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+using Quartz;
+
+namespace Infrastructure;
+
+[DisallowConcurrentExecution]
+public class LoggingBackgroundJob : IJob
+{
+    private readonly ILogger<LoggingBackgroundJob> _logger;
+    public LoggingBackgroundJob(ILogger<LoggingBackgroundJob> logger)
+    {
+        _logger = logger;
+    }
+    public Task Execute(IJobExecutionContext context)
+    {
+        _logger.LogInformation("LoggingBackgroundJob is executing."+DateTime.Now);
+        return Task.CompletedTask;
+    }
+}
---
+++ Infrastructure/LoggingBackgroundJobSetup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Options;
+using Quartz;
+
+namespace Infrastructure;
+
+public class LoggingBackgroundJobSetup : IConfigureOptions<QuartzOptions>
+{
+    public void Configure(QuartzOptions options)
+    {
+        var jobKey = JobKey.Create(nameof(LoggingBackgroundJob));
+        options
+        .AddJob<LoggingBackgroundJob>(JobBuilder=> JobBuilder.WithIdentity(jobKey))
+        .AddTrigger(trigger => trigger
+        .ForJob(jobKey)
+            //.WithCronSchedule("0/5 * * * * ?")            
+            .WithSimpleSchedule(schedule => schedule.WithIntervalInSeconds(5).RepeatForever()));
+    }
+}
